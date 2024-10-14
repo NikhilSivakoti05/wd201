c56969cd65f7ee51d55d0d8291cea1cfe036d27b
Nikhil Sivakoti <2300030644@kluniversity.in>
2024-10-14 22:02:48 +0530

    Update index.js

--- todo-cli/models/index.js
+++ todo-cli/models/index.js
@@ -1,45 +1,62 @@
-
 'use strict';
 
-const fs = require('fs');
-const path = require('path');
+// Import necessary modules
+const fileSystem = require('fs');
+const filePath = require('path');
 const Sequelize = require('sequelize');
-const process = require('process');
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
-const db = {};
+const processEnv = require('process');
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+// Get the name of the current file (index.js)
+const currentFileName = filePath.basename(__filename);
+
+// Get the environment (development by default if not set)
+const environment = processEnv.env.NODE_ENV || 'development';
+
+// Load database configuration based on the environment (development, production, etc.)
+const dbConfig = require(__dirname + '/../config/config.json')[environment];
+
+// Create an empty object to hold all database models
+const database = {};
+
+let sequelizeConnection;
+// Check if there's an environment variable for database connection
+if (dbConfig.use_env_variable) {
+  // Use the environment variable for the connection
+  sequelizeConnection = new Sequelize(processEnv.env[dbConfig.use_env_variable], dbConfig);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  // Otherwise, use the settings from the configuration file
+  sequelizeConnection = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, dbConfig);
 }
 
-fs
+// Read all files in the current directory (where models are located)
+fileSystem
   .readdirSync(__dirname)
   .filter(file => {
+    // Filter out files that start with a dot, the current file itself, and non-JS files
     return (
-      file.indexOf('.') !== 0 &&
-      file !== basename &&
-      file.slice(-3) === '.js' &&
-      file.indexOf('.test.js') === -1
+      file.indexOf('.') !== 0 && // Not a hidden file
+      file !== currentFileName && // Not the current file (index.js)
+      file.slice(-3) === '.js' && // Only files ending in .js
+      file.indexOf('.test.js') === -1 // Exclude test files
     );
   })
   .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
+    // Import each model and add it to the database object
+    const model = require(filePath.join(__dirname, file))(sequelizeConnection, Sequelize.DataTypes);
+    database[model.name] = model; // Store model in the database object
   });
 
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
+// Check each model in the database object for associations
+Object.keys(database).forEach(modelName => {
+  if (database[modelName].associate) {
+    // If an association method exists, call it
+    database[modelName].associate(database);
   }
 });
 
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+// Add the sequelize instance and Sequelize library to the database object
+database.sequelize = sequelizeConnection;
+database.Sequelize = Sequelize;
 
-module.exports = db;
-
+// Export the database object containing models and sequelize connection
+module.exports = database;
